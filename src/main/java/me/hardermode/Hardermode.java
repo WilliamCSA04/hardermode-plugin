c94ae8376b75a33b9d5644d79c1bd81719686bd7
unknown <williamcaetano14@gmail.com>
2021-05-30 01:27:04 -0300

    :sparkles: Damage increased by 20%

--- src/main/java/me/hardermode/Hardermode.java
+++ src/main/java/me/hardermode/Hardermode.java
@@ -3,7 +3,10 @@ package me.hardermode;
 import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Mob;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Hardermode extends JavaPlugin implements Listener {
@@ -14,7 +17,7 @@ public final class Hardermode extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        System.out.println("Inicializando plugin");
+        System.out.println("Starting plugin - [hardermode]");
 
         server = getServer();
         server.getPluginManager().registerEvents(this, this);
@@ -23,8 +26,13 @@ public final class Hardermode extends JavaPlugin implements Listener {
 
     }
 
+    public void onDamage(EntityDamageByEntityEvent event) {
+        double damage = event.getDamage();
+        event.setDamage(damage * 0.2);
+    }
+
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        System.out.println("Starting plugin - [hardermode]");
     }
 }
