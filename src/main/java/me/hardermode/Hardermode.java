872421485886e83d8b3e1277347cbb9b87f5568c
BuildTools <williamcaetano14@gmail.com>
2021-05-30 05:46:43 -0300

    :recycle: onCreeperExplosion

--- src/main/java/me/hardermode/Hardermode.java
+++ src/main/java/me/hardermode/Hardermode.java
@@ -6,6 +6,7 @@ import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,6 +14,7 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.loot.LootTable;
@@ -43,15 +45,29 @@ public final class Hardermode extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void destroyShieldOnCreeperExplosion(PlayerItemDamageEvent event) {
-        ItemStack item = event.getItem();
-        boolean isShield = item.getType() == Material.SHIELD;
-        if(isShield) {
-            ItemMeta itemMeta = item.getItemMeta();
-            if(itemMeta instanceof Damageable) {
-                Damageable damagedShieldMeta = (Damageable) itemMeta;
-                damagedShieldMeta.setDamage(10000);
-                item.setItemMeta((ItemMeta) damagedShieldMeta);
+    public void onCreeperExplosion(EntityDamageByEntityEvent event) {
+        boolean isCreeperExplosion = event.getDamager() instanceof Creeper;
+        if(isCreeperExplosion) {
+            boolean isPlayer = event.getEntity() instanceof Player;
+            if(isPlayer) {
+                Player player = (Player) event.getEntity();
+                if(player.isBlocking()) {
+                    ItemStack shield = null;
+                    PlayerInventory inventory = player.getInventory();
+                    if(inventory.getItemInOffHand().getType() == Material.SHIELD) {
+                        shield = inventory.getItemInOffHand();
+                    } else if(inventory.getItemInMainHand().getType() == Material.SHIELD) {
+                        shield = inventory.getItemInMainHand();
+                    }
+                    if(shield != null) {
+                        if(shield.getItemMeta() instanceof Damageable) {
+                            Damageable damagedShield = (Damageable) shield.getItemMeta();
+                            damagedShield.setDamage(750);
+                            shield.setItemMeta((ItemMeta) damagedShield);
+                        }
+                    }
+
+                }
             }
         }
     }
