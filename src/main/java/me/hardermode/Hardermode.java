d014f0f7e90a5ba0a569247fb12c30b4e7318119
unknown <williamcaetano14@gmail.com>
2021-05-30 03:00:55 -0300

    :sparkles: Created logic to destroy shield on creeper explosion

--- src/main/java/me/hardermode/Hardermode.java
+++ src/main/java/me/hardermode/Hardermode.java
@@ -3,10 +3,15 @@ package me.hardermode;
 import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Mob;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Hardermode extends JavaPlugin implements Listener {
@@ -26,9 +31,27 @@ public final class Hardermode extends JavaPlugin implements Listener {
 
     }
 
-    public void onDamage(EntityDamageByEntityEvent event) {
+    public void doubleDamage(EntityDamageByEntityEvent event) {
         double damage = event.getDamage();
         event.setDamage(damage * 0.2);
+    }
+
+    public void destroyShieldOnCreeperExplosion(EntityDamageByEntityEvent event) {
+        boolean isEntityExplosion = event.getCause() == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION;
+        boolean isCreeper = event.getDamager() instanceof Creeper;
+        if(isEntityExplosion && isCreeper) {
+            Entity entity = event.getEntity();
+            Player player = entity instanceof Player ? (Player) entity : null;
+            if(player != null) {
+                boolean isBlocking = player.isBlocking();
+                if(isBlocking) {
+                   ItemMeta shield = player.getInventory().getItemInOffHand().getItemMeta();
+                   if(shield instanceof Damageable) {
+                       ((Damageable) shield).setDamage(500);
+                   }
+                }
+            }
+        }
     }
 
     @Override
