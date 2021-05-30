c9f417b03405606a0678fb7a3c5a773a8bbe841d
BuildTools <williamcaetano14@gmail.com>
2021-05-30 05:47:59 -0300

    :fire: Delete unused imports

--- src/main/java/me/hardermode/Hardermode.java
+++ src/main/java/me/hardermode/Hardermode.java
@@ -5,20 +5,14 @@ import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Creeper;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.loot.LootTable;
-import org.bukkit.loot.LootTables;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Hardermode extends JavaPlugin implements Listener {
