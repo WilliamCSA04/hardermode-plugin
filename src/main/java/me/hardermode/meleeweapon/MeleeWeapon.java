daab7040bffe7ea94b5e1a33fb847c5ccd0b7936
BuildTools <williamcaetano14@gmail.com>
2021-06-06 23:26:58 -0300

    :sparkles: Created melee weapon

--- src/main/java/me/hardermode/meleeweapon/MeleeWeapon.java
+++ src/main/java/me/hardermode/meleeweapon/MeleeWeapon.java
@@ -1,2 +1,25 @@
-package me.hardermode.meleeweapon;public class MeleeWeapon {
+package me.hardermode.meleeweapon;
+
+import org.bukkit.Material;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MeleeWeapon {
+
+  public static List<Material> meleeWeaponMaterials() {
+    List<Material> weapons = new ArrayList<>();
+    weapons.add(Material.STONE_SWORD);
+    weapons.add(Material.WOODEN_SWORD);
+    weapons.add(Material.IRON_SWORD);
+    weapons.add(Material.DIAMOND_SWORD);
+    weapons.add(Material.NETHERITE_SWORD);
+    weapons.add(Material.STONE_AXE);
+    weapons.add(Material.WOODEN_AXE);
+    weapons.add(Material.IRON_AXE);
+    weapons.add(Material.DIAMOND_AXE);
+    weapons.add(Material.NETHERITE_AXE);
+    return weapons;
+  }
+
 }
