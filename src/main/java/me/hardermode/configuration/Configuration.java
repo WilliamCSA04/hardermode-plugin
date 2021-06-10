a9b3efe90dc8c4daf51ff71214ed52830e448a17
BuildTools <williamcaetano14@gmail.com>
2021-06-09 21:36:18 -0300

    :sparkles: Changed diamonds, golden and iron armors

--- src/main/java/me/hardermode/configuration/Configuration.java
+++ src/main/java/me/hardermode/configuration/Configuration.java
@@ -39,15 +39,46 @@ public class Configuration {
     System.out.println("[hardermode] - Ending configuration");
   }
 
+  private Recipe getRecipeFromMaterial(Material material) {
+    NamespacedKey materialKey = material.getKey();
+    ItemStack item = new ItemStack(material);
+    return new ShapedRecipe(materialKey, item);
+  }
+
+
   private void addRecipes() {
     ItemStack book = new ItemStack(Material.KNOWLEDGE_BOOK, 1);
     KnowledgeBookMeta recipeBook = (KnowledgeBookMeta) book.getItemMeta();
     Recipe enchantedGoldenApple = goldenAppleRecipe();
     Recipe conduit = conduitRecipe();
     Recipe chest = chestRecipe();
+    Recipe ironHelmet = ironHelmetRecipe();
+    Recipe goldenHelmet = goldenHelmetRecipe();
+    Recipe diamondHelmet = diamondHelmetRecipe();
+    Recipe ironChestPlate = ironChestPlateRecipe();
+    Recipe goldenChestPlate = goldenChestPlateRecipe();
+    Recipe diamondChestPlate = diamondChestPlateRecipe();
+    Recipe ironLeggings = ironLeggingsRecipe();
+    Recipe goldenLeggings = goldenLeggingsRecipe();
+    Recipe diamondLeggings = diamondLeggingsRecipe();
+    Recipe ironBoots = ironBootsRecipe();
+    Recipe goldenBoots = goldenBootsRecipe();
+    Recipe diamondBoots = diamondBootsRecipe();
     addRecipe(enchantedGoldenApple, Material.ENCHANTED_GOLDEN_APPLE, recipeBook);
     addRecipe(conduit, Material.CONDUIT, recipeBook);
     addRecipe(chest, Material.CHEST, recipeBook);
+    addRecipe(ironHelmet, Material.IRON_HELMET, recipeBook);
+    addRecipe(goldenHelmet, Material.GOLDEN_HELMET, recipeBook);
+    addRecipe(diamondHelmet, Material.DIAMOND_HELMET, recipeBook);
+    addRecipe(ironChestPlate, Material.IRON_CHESTPLATE, recipeBook);
+    addRecipe(goldenChestPlate, Material.GOLDEN_CHESTPLATE, recipeBook);
+    addRecipe(diamondChestPlate, Material.DIAMOND_CHESTPLATE, recipeBook);
+    addRecipe(ironLeggings, Material.IRON_LEGGINGS, recipeBook);
+    addRecipe(goldenLeggings, Material.GOLDEN_LEGGINGS, recipeBook);
+    addRecipe(diamondLeggings, Material.DIAMOND_LEGGINGS, recipeBook);
+    addRecipe(ironBoots, Material.IRON_BOOTS, recipeBook);
+    addRecipe(goldenBoots, Material.GOLDEN_BOOTS, recipeBook);
+    addRecipe(diamondBoots, Material.DIAMOND_BOOTS, recipeBook);
   }
 
   private void addRecipe(Recipe recipe, Material material, KnowledgeBookMeta recipeBook) {
@@ -81,6 +112,102 @@ public class Configuration {
     recipe.shape("NNN", "HHH", "NNN");
     recipe.setIngredient('H', Material.HEART_OF_THE_SEA);
     recipe.setIngredient('N', Material.NAUTILUS_SHELL);
+    return recipe;
+  }
+
+  private ShapedRecipe ironChestPlateRecipe() {
+    ShapedRecipe recipe = (ShapedRecipe) getRecipeFromMaterial(Material.IRON_CHESTPLATE);
+    recipe.shape("ICI", "III", "III");
+    recipe.setIngredient('C', Material.LEATHER_CHESTPLATE);
+    recipe.setIngredient('I', Material.IRON_INGOT);
+    return recipe;
+  }
+
+  private ShapedRecipe goldenChestPlateRecipe() {
+    ShapedRecipe recipe = (ShapedRecipe) getRecipeFromMaterial(Material.GOLDEN_CHESTPLATE);
+    recipe.shape("ICI", "III", "III");
+    recipe.setIngredient('C', Material.LEATHER_CHESTPLATE);
+    recipe.setIngredient('I', Material.GOLD_INGOT);
+    return recipe;
+  }
+
+  private ShapedRecipe diamondChestPlateRecipe() {
+    ShapedRecipe recipe = (ShapedRecipe) getRecipeFromMaterial(Material.DIAMOND_CHESTPLATE);
+    recipe.shape("ICI", "III", "III");
+    recipe.setIngredient('C', Material.LEATHER_CHESTPLATE);
+    recipe.setIngredient('I', Material.DIAMOND);
+    return recipe;
+  }
+
+  private ShapedRecipe ironLeggingsRecipe() {
+    ShapedRecipe recipe = (ShapedRecipe) getRecipeFromMaterial(Material.IRON_LEGGINGS);
+    recipe.shape("III", "ICI", "I I");
+    recipe.setIngredient('C', Material.LEATHER_LEGGINGS);
+    recipe.setIngredient('I', Material.IRON_INGOT);
+    return recipe;
+  }
+
+  private ShapedRecipe goldenLeggingsRecipe() {
+    ShapedRecipe recipe = (ShapedRecipe) getRecipeFromMaterial(Material.GOLDEN_LEGGINGS);
+    recipe.shape("III", "ICI", "I I");
+    recipe.setIngredient('C', Material.LEATHER_LEGGINGS);
+    recipe.setIngredient('I', Material.GOLD_INGOT);
+    return recipe;
+  }
+
+  private ShapedRecipe diamondLeggingsRecipe() {
+    ShapedRecipe recipe = (ShapedRecipe) getRecipeFromMaterial(Material.DIAMOND_LEGGINGS);
+    recipe.shape("III", "ICI", "I I");
+    recipe.setIngredient('C', Material.LEATHER_LEGGINGS);
+    recipe.setIngredient('I', Material.DIAMOND);
+    return recipe;
+  }
+
+  private ShapedRecipe ironHelmetRecipe() {
+    ShapedRecipe recipe = (ShapedRecipe) getRecipeFromMaterial(Material.IRON_HELMET);
+    recipe.shape("III", "ICI", "  ");
+    recipe.setIngredient('C', Material.LEATHER_HELMET);
+    recipe.setIngredient('I', Material.IRON_INGOT);
+    return recipe;
+  }
+
+  private ShapedRecipe goldenHelmetRecipe() {
+    ShapedRecipe recipe = (ShapedRecipe) getRecipeFromMaterial(Material.GOLDEN_HELMET);
+    recipe.shape("III", "ICI", "  ");
+    recipe.setIngredient('C', Material.LEATHER_HELMET);
+    recipe.setIngredient('I', Material.GOLD_INGOT);
+    return recipe;
+  }
+
+  private ShapedRecipe diamondHelmetRecipe() {
+    ShapedRecipe recipe = (ShapedRecipe) getRecipeFromMaterial(Material.DIAMOND_HELMET);
+    recipe.shape("III", "ICI", "  ");
+    recipe.setIngredient('C', Material.LEATHER_HELMET);
+    recipe.setIngredient('I', Material.DIAMOND);
+    return recipe;
+  }
+
+  private ShapedRecipe ironBootsRecipe() {
+    ShapedRecipe recipe = (ShapedRecipe) getRecipeFromMaterial(Material.IRON_BOOTS);
+    recipe.shape("  ", "ICI", "I I");
+    recipe.setIngredient('C', Material.LEATHER_BOOTS);
+    recipe.setIngredient('I', Material.IRON_INGOT);
+    return recipe;
+  }
+
+  private ShapedRecipe goldenBootsRecipe() {
+    ShapedRecipe recipe = (ShapedRecipe) getRecipeFromMaterial(Material.GOLDEN_BOOTS);
+    recipe.shape("  ", "ICI", "I I");
+    recipe.setIngredient('C', Material.LEATHER_BOOTS);
+    recipe.setIngredient('I', Material.GOLD_INGOT);
+    return recipe;
+  }
+
+  private ShapedRecipe diamondBootsRecipe() {
+    ShapedRecipe recipe = (ShapedRecipe) getRecipeFromMaterial(Material.DIAMOND_BOOTS);
+    recipe.shape("  ", "ICI", "I I");
+    recipe.setIngredient('C', Material.LEATHER_BOOTS);
+    recipe.setIngredient('I', Material.DIAMOND);
     return recipe;
   }
 
